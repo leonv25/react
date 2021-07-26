705fe0ef3392a7d1651af7dbccde1b6aeb11eab5
LeonV <leonvv25@gmail.com>
2021-07-26 22:47:55 +0300

    commit_26_1/07/2021

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+function Greet() {
+  return(
+  <h1>Hello World!</h1>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Greet />
   </React.StrictMode>,
   document.getElementById('root')
 );
