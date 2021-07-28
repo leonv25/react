5179ece74c848c4f55fec3b4d3a3b0786da26108
LeonV <leonvv25@gmail.com>
2021-07-29 02:12:49 +0300

    commit_29/07/2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ShowBanner(props) {
+  if (props.time > 45) {
+    return(
+      <div className="rest_block">Отдих</div>
+    )
+    
+  } else {
+    return(
+      <div className="work_block">Время работи над собой!</div>
+    )
+  }
+}
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()} 
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+
+  render() {
+    return(
+        <div>
+          <ShowBanner time={this.state.date.getSeconds()} />
+          <h1>Текущее время {this.state.date.toLocaleDateString()}</h1>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Clock from './App';
 import reportWebVitals from './reportWebVitals';
 
 class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
+        <Clock />
         
       </div>
     )
   }
 }
+
 
 ReactDOM.render(
   <React.StrictMode><App /></React.StrictMode>,
